6e41576724feae04ec545ffd78c6642cb5a0c1e2
Derek Ness <nessd1997@gmail.com>
2020-03-19 11:03:40 -0400

    IDialogueItem example. Starts dialogue text and gets value from a gameObject that has a component that implements IDialogueItem.
    Replacing the old way on how card information was retrieved. Also split out controls for the DIalogue box to its own controller.

---
+++ Dioscuri/Assets/Components/Controllers/Scripts/DialogueController.cs
@@ -0,0 +1,53 @@
+﻿using Assets.Components.DialogueBox.Scripts;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+// This goes on same component as PlayerController
+public class DialogueController : MonoBehaviour
+{
+    private bool _uiDisplayOpen = false;
+    private IDialogueItem _dialogueItem;
+
+    #region Control Methods
+    // Update is called once per frame
+    void Update()
+    {
+        // would live in PlayerController
+        if (!_uiDisplayOpen && Input.GetKeyDown(KeyCode.D)) {
+            _dialogueItem = GetComponent<IDialogueItem>();
+            OpenDialoguBox(_dialogueItem);
+        }
+
+        //if (!_uiDisplayOpen) return;
+
+        if (Input.GetKeyDown(KeyCode.Return) && !DialogueWriter.IsTyping)
+            NextDialogue();
+        if (Input.GetKeyDown(KeyCode.Space))
+            CloseDialogueBox();
+    }
+
+    private void NextDialogue() {
+        if(_dialogueItem.HasNextCard())
+        DialogueBoxNotifier.PrintDialogueCard(_dialogueItem.GetNextDialogueCard());
+    }
+
+    private void CloseDialogueBox() {
+        _uiDisplayOpen = false;
+        DialogueBoxNotifier.CloseDialogueBox();
+        // enable playerController
+        enabled = false;
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Opens the DialogueBox and plays out the provided DialogueItem
+    /// </summary>
+    /// <param name="dialogueItem"></param>
+    public void OpenDialoguBox(IDialogueItem dialogueItem) {
+        _dialogueItem = dialogueItem;
+        DialogueBoxNotifier.OpenDialoguBox();
+        _uiDisplayOpen = true;
+    }
+}
+
---
+++ Dioscuri/Assets/DialogueHolder.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Components.DialogueBox.Scripts;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour, IDialogueItem
+{
+    public Sprite CardSprite;
+    public Sprite CardSprite2;
+
+    private DialogueCard[] dialogueCards;
+    private int currentIndex = 0;
+
+    #region IDialogueItem Implementation
+    public DialogueCard GetNextDialogueCard() {
+        currentIndex++;
+        return dialogueCards[currentIndex];
+    }
+
+    public bool HasNextCard() {
+        return currentIndex < dialogueCards.Length;
+    }
+    #endregion
+
+    void Start() { 
+        dialogueCards = new DialogueCard[]{
+            new DialogueCard { CharacterPortrait = CardSprite, CharacterName = "Viktor", DialogueText = "This is some text 1"},
+            new DialogueCard { CharacterPortrait = CardSprite2, CharacterName = "Markus", DialogueText = "This is some text 2"},
+            new DialogueCard { CharacterPortrait = CardSprite, CharacterName = "Viktor", DialogueText = "This is some text 3"},
+            new DialogueCard { CharacterPortrait = CardSprite2, CharacterName = "Markus", DialogueText = "THis is some text 4"}
+    };
+    }
+}
